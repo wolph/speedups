72e0978aa3710056fc93c1cc8f50b3acf43151a1
Rick van Hattem <Wolph@wol.ph>
2023-08-22 00:58:05 +0200

    added ruff/flake8/pyright/codespell/black compliance and tox configurations

--- speedups/__about__.py
+++ speedups/__about__.py
@@ -3,11 +3,12 @@ __import_name__ = 'speedups'
 __version__ = '1.2.0'
 __author__ = 'Rick van Hattem, Joren Hammudoglu'
 __author_email__ = 'Wolph@Wol.ph, jhammudoglu@gmail.com'
-__description__ = ' '.join('''
+__description__ = ' '.join(
+    '''
 Library with some C and Cython code for speeding up common operations.
 
 This is externalized to avoid the hassle of building binary wheels in my other
 projects.
-'''.split())
+'''.split()
+)
 __url__ = 'https://github.com/WoLpH/speedups/'
-
