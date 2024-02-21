8ec9e136a73849d4833c103779ba3a97cae41ad1
Rick van Hattem <Wolph@wol.ph>
2024-02-21 12:37:23 +0100

    Incrementing version to v1.3.2

--- speedups/__about__.py
+++ speedups/__about__.py
@@ -1,6 +1,6 @@
 __package_name__ = 'speedups'
 __import_name__ = 'speedups'
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 __author__ = 'Rick van Hattem, Joren Hammudoglu'
 __author_email__ = 'Wolph@Wol.ph, jhammudoglu@gmail.com'
 __description__ = ' '.join(
