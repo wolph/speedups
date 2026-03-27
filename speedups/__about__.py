d877b2ec12d48366b8bc1b9ed9438af59ace3d03
Rick van Hattem <wolph@wol.ph>
2026-03-27 04:46:20 +0100

    Address PR #9 review: use PackageNotFoundError, single-source package name

--- speedups/__about__.py
+++ speedups/__about__.py
@@ -1,12 +1,14 @@
+from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as _version
-
-try:
-    __version__: str = _version('speedups')
-except Exception:
-    __version__ = '0.0.0'
 
 __package_name__ = 'speedups'
 __import_name__ = 'speedups'
+
+try:
+    __version__: str = _version(__package_name__)
+except PackageNotFoundError:
+    __version__ = '0.0.0'
+
 __author__ = 'Rick van Hattem, Joren Hammudoglu'
 __author_email__ = 'Wolph@Wol.ph, jhammudoglu@gmail.com'
 __description__ = (
