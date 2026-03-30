674f4cd56f24f1bde462b2169328369fea672e44
Rick van Hattem <wolph@wol.ph>
2026-03-30 04:49:28 +0200

    Migrate build system from setuptools+tox to uv
    - Replace setup.cfg with full pyproject.toml project metadata
    - Modernize setup.py (retain for Cython compilation)
    - Remove tox.ini in favor of [tool.tox] in pyproject.toml
    - Add uv.lock for reproducible dependency resolution
    - Consolidate optional-dependencies extras (test, lint, typecheck, dev)
    - Use importlib.metadata for dynamic version in __about__.py

--- speedups/__about__.py
+++ speedups/__about__.py
@@ -1,14 +1,19 @@
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _version
+
 __package_name__ = 'speedups'
 __import_name__ = 'speedups'
-__version__ = '1.4.0'
+
+try:
+    __version__: str = _version(__package_name__)
+except PackageNotFoundError:
+    __version__ = '0.0.0'
+
 __author__ = 'Rick van Hattem, Joren Hammudoglu'
 __author_email__ = 'Wolph@Wol.ph, jhammudoglu@gmail.com'
-__description__ = ' '.join(
-    '''
-Library with some C and Cython code for speeding up common operations.
-
-This is externalized to avoid the hassle of building binary wheels in my other
-projects.
-'''.split()
+__description__ = (
+    'Library with some C and Cython code for speeding up'
+    ' common operations. This is externalized to avoid the'
+    ' hassle of building binary wheels in my other projects.'
 )
 __url__ = 'https://github.com/WoLpH/speedups/'
